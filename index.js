09fbed25586d2e570dc00b852c05189f8c64fc96
Artem Berezin <berezin.artem@gmail.com>
2016-11-22 00:29:01 +1000

    render asyncrchrous

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
 'use strict';
 const anymatch = require('anymatch');
+const sass = require('node-sass');
+const path = require('path');
+
 const defaultPassthrough = /^node_modules/;
-var sass = require('node-sass');
 
 class InlineScssCompiler {
-  constructor(config) {
-    this.config = config && config.plugins && config.plugins.inlineScss || {};
-    this.sassOptions = this.config.options || {};
+    constructor(config) {
+        this.config = config && config.plugins && config.plugins.inlineScss || {};
+        this.sassOptions = this.config.options || {};
+        if (this.config.pattern) {
+            this.pattern = this.config.pattern;
+        }
+        if (this.config.passthrough === false) {
+            this.passthrough = () => false;
+        } else {
+            this.passthrough = anymatch(this.config.passthrough || defaultPassthrough);
+        }
+    }
 
-    if (this.config.pattern) {
-      this.pattern = this.config.pattern;
+    compile(file) {
+        if (this.passthrough(file.path)) {
+            return Promise.resolve(file);
+        }
+
+        const sassOptions = Object.assign({
+            data: file.data,
+            includePaths: [
+                path.dirname(file.path)
+            ]
+        }, this.sassOptions);
+
+        return this._renderScss(sassOptions)
+            .then(function (result) {
+                const payload = JSON.stringify(result.css.toString('utf8'));
+                const exports = 'module.exports = ' + payload;
+                return {
+                    exports,
+                    path: file.path,
+                    data: ''
+                };
+            })
     }
-    if (this.config.passthrough === false) {
-      this.passthrough = () => false;
-    } else {
-      this.passthrough = anymatch(this.config.passthrough || defaultPassthrough);
+
+    _renderScss(options) {
+        return new Promise(function (resolve, reject) {
+            sass.render(options, function (err, result) {
+                if (err) {
+                    reject(err)
+                }
+                resolve(result);
+            });
+        });
     }
-  }
-  
-  compile(file) {
-    try {
-      if (this.passthrough(file.path)) {
-        return Promise.resolve(file);
-      }
-      const css = sass.renderSync(Object.assign({data: file.data}, this.sassOptions)).css;
-      const payload = JSON.stringify(css.toString('utf8'));
-      const exports = 'module.exports = ' + payload;
-      return Promise.resolve({exports, path: file.path, data: ''});
-    } catch (err) {
-      return Promise.reject(err);
-    }
-  }
 }
 
 InlineScssCompiler.prototype.brunchPlugin = true;
